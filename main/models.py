6385d4e764231f714a26668932e333653f017847
Andrey Panov <AnPanov@alabuga.ru>
2025-11-07 15:51:44 +0300

    сделал views, настроил urls, написал шаблоны каталога

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -9,6 +10,9 @@ class Category(models.Model):
         ordering = ('name',)
         verbose_name='Категория'
         verbose_name_plural='Категории'
+
+    def get_absolute_url(self):
+        return reverse("main:product_list_by_category", args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -29,8 +33,12 @@ class Product(models.Model):
         ordering = (
             'name',
         )
-        def __str__(self):
-            return self.name
+
+    def get_absolute_url(self):
+        return reverse("main:product_detail", args=[self.id,self.slug])
+
+    def __str__(self):
+        return self.name
 
   
 
